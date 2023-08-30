56d8654311cfd10932242e4f7c57ab84b84b502c
dallyh <hon.dalibor@gmail.com>
2023-08-30 23:50:41 +0200

    Add site and base to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dallyh.github.io',
+  base: '/website-concept',
   integrations: [react(), astroI18next()],
 });
