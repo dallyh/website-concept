536e3daf1e6c8c5f060521019c738d7b366c35b2
dallyh <hon.dalibor@gmail.com>
2023-11-29 22:08:02 +0100

    Contact form fixes and additions

--- src/components/react/ContactForm/ContactForm.tsx
+++ src/components/react/ContactForm/ContactForm.tsx
@@ -1,8 +1,8 @@
-import { useState, useEffect, useRef, type FormEvent } from "react";
+import { useState, useEffect, useRef, type FocusEvent } from "react";
 import { useForm } from "@formspree/react";
-import autoAnimate from "@formkit/auto-animate";
 import "./ContactForm.css";
 import type { ContactForm } from "../../../i18n/locales";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 interface ContactFormProps {
     resources: ContactForm;
@@ -14,8 +14,41 @@ const contactForm = (props: ContactFormProps) => {
     const [state, handleSubmit] = useForm("mayzkojd");
     const [errorState, setErrorMessage] = useState([{ type: "", message: "" }]);
     const isFirstRender = useRef(true);
-    const formRef = useRef(null);
-    const formWrapperRef = useRef(null);
+    const [formRef] = useAutoAnimate();
+    const [formWrapperRef] = useAutoAnimate();
+
+    // Validate form submission errors
+    const validateError = (errorCode: string, errorMessage: string) => {
+        switch (errorCode) {
+            case "TYPE_EMAIL":
+                setErrorMessage((prevState) => [
+                    ...prevState,
+                    {
+                        type: "EMAIL",
+                        message: props.resources.ERROR_TYPE_EMAIL,
+                    },
+                ]);
+                break;
+            case "TYPE_TEXT":
+                setErrorMessage((prevState) => [
+                    ...prevState,
+                    {
+                        type: "TEXT",
+                        message: props.resources.ERROR_TYPE_TEXT,
+                    },
+                ]);
+                break;
+            default:
+                setErrorMessage((prevState) => [
+                    ...prevState,
+                    {
+                        type: errorCode ?? "UNDEFINED",
+                        message: errorMessage,
+                    },
+                ]);
+                break;
+        }
+    };
 
     // Handle submit and errors
     useEffect(() => {
@@ -52,80 +85,43 @@ const contactForm = (props: ContactFormProps) => {
         ]);
     }, [state.errors]);
 
-    // Animate form
-    useEffect(() => {
-        formRef.current && autoAnimate(formRef.current);
-        formWrapperRef.current && autoAnimate(formWrapperRef.current);
-    }, [formRef, formWrapperRef]);
+    const selectText = (event: FocusEvent<HTMLElement>) => {
+        if (event.target instanceof HTMLTextAreaElement) {
+            let elem = event.target as HTMLTextAreaElement;
+            elem.select();
+        }
 
-    // Validate form submission errors
-    const validateError = (errorCode: string, errorMessage: string) => {
-        switch (errorCode) {
-            case "TYPE_EMAIL":
-                setErrorMessage((prevState) => [
-                    ...prevState,
-                    {
-                        type: "EMAIL",
-                        message: props.resources.ERROR_TYPE_EMAIL,
-                    },
-                ]);
-                break;
-            case "TYPE_TEXT":
-                setErrorMessage((prevState) => [
-                    ...prevState,
-                    {
-                        type: "TEXT",
-                        message: props.resources.ERROR_TYPE_TEXT,
-                    },
-                ]);
-                break;
-            default:
-                setErrorMessage((prevState) => [
-                    ...prevState,
-                    {
-                        type: errorCode ?? "UNDEFINED",
-                        message: errorMessage,
-                    },
-                ]);
-                break;
+        if (event.target instanceof HTMLInputElement) {
+            let elem = event.target as HTMLInputElement;
+            elem.select();
         }
     };
 
     return (
         <div ref={formWrapperRef}>
-            {state.submitting && (
-                <div id="submitting">
-                    <div className="lds-ring">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                </div>
-            )}
             {state.succeeded && (
                 <div id="submit-success">
                     <p>{props.resources.SubmitSuccess}</p>
                 </div>
             )}
-            {!state.succeeded && !state.submitting && (
+            {!state.succeeded && (
                 <form id="fs-frm" ref={formRef} name="simple-contact-form" onSubmit={handleSubmit}>
-                    <fieldset id="fs-frm-inputs">
+                    <fieldset id="fs-frm-inputs" disabled={state.submitting}>
                         <input type="hidden" name="_language" value="cs" />
                         <div className="contact-info-container">
                             <label htmlFor="full-name">{props.resources.FullName}</label>
-                            <input type="text" autoComplete="name" name="name" id="full-name" placeholder={props.resources.NameAndSurname} required />
+                            <input type="text" autoComplete="name" name="name" id="full-name" placeholder={props.resources.NameAndSurname} required onFocus={selectText} />
                             <label htmlFor="email">{props.resources.EmailAddress}</label>
-                            <input id="email" autoComplete="email" type="email" name="email" placeholder="@" required />
                             {errorState.some((key) => key.type === "EMAIL") && <p className="error">{errorState.find((item) => item.type === "EMAIL")?.message}</p>}
-                            <label htmlFor="email-subject">Subject</label>
-                            <input type="text" name="_subject" id="email-subject" placeholder="Subject" defaultValue="New resume submission" required />
+                            <input id="email" autoComplete="email" type="email" name="email" placeholder="@" required onFocus={selectText} />
+                            <label htmlFor="email-subject">{props.resources.Subject}</label>
+                            <input type="text" name="_subject" id="email-subject" placeholder={props.resources.Subject} defaultValue={props.resources.SubjectDefault} required onFocus={selectText} />
                         </div>
                         <div className="message-container">
                             <label htmlFor="message">{props.resources.Message}</label>
                             <div className="textarea-container">
                                 <div className="textarea-bg" />
-                                <textarea name="message" id="message" placeholder={props.resources.MessagePlaceholder} required />
+                                <textarea name="message" id="message" placeholder={props.resources.MessagePlaceholder} required onFocus={selectText} />
                             </div>
                             {errorState.some((key) => key.type === "TEXT") && <p className="error">{errorState.find((item) => item.type === "TEXT")?.message}</p>}
                         </div>
@@ -139,7 +135,17 @@ const contactForm = (props: ContactFormProps) => {
                             </button>
                         )}
                         <button type="submit" id="fs-frm-submit-button" className="button" disabled={state.submitting}>
-                            {props.resources.Submit}
+                            {!state.submitting && props.resources.Submit}
+                            {state.submitting && (
+                                <div id="submitting">
+                                    <div className="lds-ring">
+                                        <div></div>
+                                        <div></div>
+                                        <div></div>
+                                        <div></div>
+                                    </div>
+                                </div>
+                            )}
                         </button>
                     </div>
                 </form>
