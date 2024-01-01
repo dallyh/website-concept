d35e1b9df1df077573ac9fdc19c5abf549da0091
Dalibor Hon <hon.dalibor@gmail.com>
2024-01-01 13:22:42 +0100

    Add workflow to deploy site PR previews to another repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import { loadEnv } from "vite";
+const { SITE_URL, SITE_BASE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // i18n
 const defaultLocale = "cs";
@@ -12,8 +14,8 @@ const locales = {
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://dallyh.github.io",
-    base: "/website-concept",
+    site: SITE_URL,
+    base: SITE_BASE,
     trailingSlash: "always",
     build: {
         format: "directory",
